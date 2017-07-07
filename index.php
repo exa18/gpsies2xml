e6610a7aef57a4e299c4683758c2ebf531f26723
Julian <julian.cenkier@wp.eu>
2017-07-07 10:09:11 +0200

    1.1 update
    added mobile scaling metadata

--- index.php
+++ index.php
@@ -132,6 +132,8 @@ exit();
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 
